e61fac4a1e35ffe72d1c029faa7e0179184b5a04
yaignis <yakubets.vadim@yahoo.fr>
2025-03-21 15:37:07 +0000

    The first version of find password

---
+++ passwd_find.py
@@ -0,0 +1,23 @@
+import random
+import time
+mot_de_passe = input("Choisir le mot de passe: ")  # le mot de passe à trouver
+
+
+def mot_aleatoire(longueur):
+    lettres = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+               'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    mot_genere = ""
+    for carac in range(0, longueur):
+        mot_genere = mot_genere + lettres[random.randint(0, len(lettres) - 1)]
+    return mot_genere
+
+
+debut = time.time()
+while True:
+    mot_alea = mot_aleatoire(len(mot_de_passe))
+    print("Mot de passe teste: " + mot_alea)
+    if mot_de_passe == mot_alea:
+        print("Mot de passe trouve: " + mot_alea)
+        fin = time.time() - debut
+        print("Trouve en " + str(fin) + " second")
+        break
